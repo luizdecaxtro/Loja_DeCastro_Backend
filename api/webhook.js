b6b3a9840bdd0e968ae3e81f81cc798eac6aaaba
DeCastro <97640132+luizdecaxtro@users.noreply.github.com>
2025-09-16 16:57:37 -0300

    Delete api directory

--- api/webhook.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-  try {
-    console.log("Webhook recebido:", req.body);
-    res.status(200).send("OK");
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
