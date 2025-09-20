c54e34aac747e9eb2746a4deb63169999925f428
DeCastro <97640132+luizdecaxtro@users.noreply.github.com>
2025-09-20 09:12:29 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -30,10 +30,7 @@ const sobreFilePath = path.join(__dirname, 'data', 'sobre.json');
 // CONFIGURAÇÃO DO MULTER
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        // Assegure-se de que a pasta 'uploads' existe antes de tentar salvar
-        fs.mkdir(path.join(__dirname, 'uploads'), { recursive: true }).then(() => {
-            cb(null, path.join(__dirname, 'uploads/'));
-        });
+        cb(null, path.join(__dirname, 'uploads/'));
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`);
@@ -65,22 +62,7 @@ app.get('/api/produtos', async (req, res) => {
 
 app.post('/api/produtos', upload.single('imagem'), async (req, res) => {
     try {
-        console.log('Recebendo requisição para adicionar produto...');
-        console.log('Corpo da requisição:', req.body);
-        console.log('Arquivo recebido:', req.file);
-
-        let produtos = [];
-        try {
-            const data = await fs.readFile(produtosFilePath, 'utf8');
-            produtos = JSON.parse(data);
-        } catch (readError) {
-            if (readError.code === 'ENOENT') {
-                console.log('Arquivo de produtos não encontrado. Criando um novo.');
-            } else {
-                throw readError;
-            }
-        }
-        
+        const produtos = JSON.parse(await fs.readFile(produtosFilePath, 'utf8'));
         const { nome, preco, descricao } = req.body;
         const novoProduto = {
             id: Date.now(),
